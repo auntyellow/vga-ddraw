14e7d2a4a547fb08e66523a77ca9227f1dadc0c8
auntyellow <webmaster@xqbase.com>
2026-05-26 23:48:03 +0800

    remove unused libs

--- dsound.c
+++ dsound.c
@@ -1,7 +1,5 @@
 #include <windows.h>
 
-extern HRESULT WINAPI DirectSoundCreate(LPVOID arg1, LPVOID arg2, LPVOID arg3);
-
 HRESULT WINAPI DirectSoundCreate(LPVOID arg1, LPVOID arg2, LPVOID arg3) {
     return E_NOTIMPL;
 }
